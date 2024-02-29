ec6b6e8ac8379677eea38a319b3182666043a2f0
Daisy Tonui <tonuidaisy@gmail.com>
2024-02-29 14:39:30 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
-	}
+		size_t l = 0, r = 0;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (1 + (left_height > right_height ? left_height : right_height));
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
